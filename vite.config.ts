f59efe70522e4cd0c00df37c932806283e1d999c
Daniel G. Taylor <dtaylor@istreamplanet.com>
2023-03-08 19:09:48 -0800

    feat: cleanup deps, visualize bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig, type PluginOption } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), visualizer({ gzipSize: true }) as PluginOption],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
